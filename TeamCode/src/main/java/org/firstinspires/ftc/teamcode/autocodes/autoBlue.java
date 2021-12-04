c4605fb1de733f8ef2ec1443379a3e67dd4ca80f
abush2024 <73666145+abush2024@users.noreply.github.com>
2021-12-03 18:57:36 -0800

    were screwed help

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocodes/autoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocodes/autoBlue.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import java.lang.*;
 
 @Autonomous
 public class autoBlue extends LinearOpMode {
