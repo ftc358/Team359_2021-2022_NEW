96f108ab2b2b1fb4885e899fbbde4b02ff221fcc
ZopesPrograms <72052181+ZopesPrograms@users.noreply.github.com>
2021-12-16 16:46:03 -0800

    Update AUTO_START_RED_SEBAS.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO_START_RED_SEBAS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO_START_RED_SEBAS.java
@@ -7,6 +7,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import java.lang.*;
