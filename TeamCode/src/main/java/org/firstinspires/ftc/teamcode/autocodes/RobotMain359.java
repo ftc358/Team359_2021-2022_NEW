7626d622992800b324580974f102a95ed535b8a0
abush2024 <73666145+abush2024@users.noreply.github.com>
2022-01-12 16:20:07 -0800

    Revert "aljdncralnce"
    This reverts commit c6e92ab2b622336cd6f178eb4658f00d15b11929.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocodes/RobotMain359.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autocodes/RobotMain359.java
@@ -72,112 +72,29 @@ public abstract class RobotMain359 extends LinearOpMode {
         return df;
     }
 
-    public void forward (double time, float power){
+    public void forward (long time, float power){
         motorLeft.setPower(power);
         motorRight.setPower(power);
-        sleep((double) (time/1000.0);
+        sleep(time/1000);
 
     }
 
     public void turn (int degree, double power){
-        int ticks = (int) (degree * TURN_FACTOR);
 
-        //Reset Encoders
-        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        //Set Target Position
-        lf.setTargetPosition(lf.getCurrentPosition() - ticks);
-        lb.setTargetPosition(lb.getCurrentPosition() - ticks);
-        rf.setTargetPosition(rf.getCurrentPosition() + ticks);
-        rb.setTargetPosition(rb.getCurrentPosition() + ticks);
 
-        //Set Drive Power
-        lf.setPower(0.5 * power);
-        lb.setPower(0.5 * power);
-        rf.setPower(0.5 * power);
-        rb.setPower(0.5 * power);
-
-        //Set to RUN_TO_POSITION mode
-        lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        while (lf.isBusy() && lb.isBusy() && rf.isBusy() && rb.isBusy()){
-            telemetry.addData("lf", -lf.getCurrentPosition());
-            telemetry.addData("rf", -rf.getCurrentPosition());
-            telemetry.addData("lb", -lb.getCurrentPosition());
-            telemetry.addData("rb", -rb.getCurrentPosition());
-            telemetry.update();
-            //Wait Until Target Position is Reached
-        }
-        sleep(500);
     }
 
     public void carousel(String state){
 
-        //Reset Encoders
-        crMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        //Set Target Position
-        crMotor.setTargetPosition(crMotor.getCurrentPosition() + 3000);
-
-        //Set Drive Power
-        if (state == "red") {
-            crMotor.setPower(0.3);
-        } else if (state == "blue") {
-            crMotor.setPower(-0.3);
-        }
-
-        //Set to RUN_TO_POSITION mode
-        crMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        while (crMotor.isBusy()){
-        }
     }
 
     public void slide(int position){
-        /**
-         * LEVEL 1 = 500
-         * LEVEL 2 = 1000
-         * MAX / LEVEL 3 = 1700
-         * */
-        //Set Target Position
-        slideMotor.setTargetPosition(slideMotor.getCurrentPosition() + position);
 
-        //Set Drive Power
-        if (position > slideMotor.getCurrentPosition()) {
-            slideMotor.setPower(0.5);
-        } else if (position < slideMotor.getCurrentPosition()){
-            slideMotor.setPower(-0.3);
-        }
-
-
-        //Set to RUN_TO_POSITION mode
-        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        while (slideMotor.isBusy()){
-        }
     }
 
-    /* public void intake (){
-        //Reset Encoders
-        crMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    public void intake (){
 
-        //Set Target Position
-        crMotor.setTargetPosition(crMotor.getCurrentPosition() + 2000);
-
-        crMotor.setPower(0.5);
-
-        //Set to RUN_TO_POSITION mode
-        crMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        while (crMotor.isBusy()){
-            intakeServo.setPower(0.3);
-        }
     }
-*/
+
 }
